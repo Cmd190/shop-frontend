fb83a5d1eca81a6f5aefd8a39d58c12b7c2f1847
Cmd190 <tonigaser@gmail.com>
2025-09-10 15:45:46 +0200

    bug with routing

--- app/routes.ts
+++ app/routes.ts
@@ -26,7 +26,8 @@ export const RoutePaths = (name:RouteName) : string =>  {
 export default [
     index("routes/home.tsx"),
     route("/products", "routes/ProductDemo.tsx"),
-    route("products/:productLink", "routes/ProductDetail.tsx"),
+    // TODO why does this not work?
+    route("/products/:productLink", "routes/ProductDetail.tsx"),
     route("/milk-chocolate", "routes/MilkChocolate.tsx"),
     route("/dark-chocolate", "routes/DarkChocolate.tsx"),
     route("/white-chocolate", "routes/WhiteChocolate.tsx"),
